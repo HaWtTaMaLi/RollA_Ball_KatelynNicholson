5d015003b920a42f4b381e3f456423dd626e8f47
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-09-27 11:53:22 -0300

    Particles!
    added particle system to my pickup item

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public AudioClip pickupSound;   // Drag sound file here in Inspector
 
     void Update()
     {
+        // Keep spinning
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Play the sound at the pickup's position
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+
+            // Destroy the pickup
+            Destroy(gameObject);
+        }
+    }
 }
