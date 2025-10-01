7cbc7e2398179e1d8c84ccebffd6d6446587b9ca
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-01 16:48:33 -0300

    AI Agent Fixed
    Thank you Michael Tweedle

--- RollA_Ball_KatelynNicholson-main/Assets/Scripts/PlayerController.cs
+++ RollA_Ball_KatelynNicholson-main/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,9 +15,13 @@ public class PlayerController : MonoBehaviour
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
-
     void Start()
     {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+
         rb = GetComponent<Rigidbody>();
 
         count = 0;
@@ -23,6 +29,17 @@ public class PlayerController : MonoBehaviour
 
         winTextObject.SetActive(false);
 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) //if the escape key is pressed then set active Pause Menu UI
+        {
+            PauseMenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        }
     }
 
     void OnMove(InputValue movementValue)
@@ -63,6 +80,11 @@ public class PlayerController : MonoBehaviour
             winTextObject.gameObject.SetActive(true);
             winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
         }
+
+        if (collision.gameObject.CompareTag("PickUp"))
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -75,5 +97,46 @@ public class PlayerController : MonoBehaviour
             SetCountText();
 
         }
+    }
+
+    public GameObject PauseMenu;
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PauseEscape()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
