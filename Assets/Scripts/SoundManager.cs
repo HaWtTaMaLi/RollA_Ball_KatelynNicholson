fb90e4c7021dd49a73e18811a952c59c32861782
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-27 21:53:33 -0300

    Got volume settings almost complete
    just need to fix a weird error with level 2 player twice the music

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public enum SoundType
 {
@@ -14,20 +16,60 @@ public enum SoundType
 }
 
 [RequireComponent(typeof(AudioSource))]
+
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource hoverSound;
+    public AudioSource clickSound;
+
     [SerializeField] private AudioClip[] soundList;
-    private static SoundManager instance;
+    public static SoundManager instance;
     private AudioSource audioSource;
+
+    [SerializeField] public AudioMixer AudioMixer;
+    [SerializeField] public Slider musicSlider;
+    [SerializeField] public Slider sfxSlider;
 
     private void Awake()
     {
-        instance = this;
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+
+         if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        AudioMixer.SetFloat("Music", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        AudioMixer.SetFloat("SFX", volume);
+    }
+
+    public void PlayHover()
+    {
+        hoverSound.Play();
+    }
+
+    public void PlayClick()
+    {
+        clickSound.Play();
     }
 
     public static void PlaySound(SoundType sound, float volume = 1)
